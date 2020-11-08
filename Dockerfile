0c9982c7c652422117b3f7df459753639f4b5111
Jon Mohrbacher <johnnymo87@gmail.com>
2020-11-08 13:23:34 -0500

    Get initial imports working

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,8 @@
 FROM python:3.8.3-buster AS base
 
 COPY requirements.txt /tmp/requirements.txt
-RUN  pip3 install -r /tmp/requirements.txt
+RUN  pip3 install --upgrade pip && \
+  pip3 install -r /tmp/requirements.txt
 
 # Release image
 FROM base AS release
@@ -11,11 +12,6 @@ WORKDIR /app
 COPY . /app
 ENV PYTHONPATH=.
 
-CMD python3 .
-
-# Test image
-FROM release AS test
-
 RUN ln -sf /app/.python_history ~/.python_history
 
-CMD pytest
+CMD python3 .
--- __main__.py
+++ __main__.py
@@ -1,4 +1,13 @@
-from lib.hello import Hello
+# Pandas to read csv file and other things
+import pandas as pd
+# Datareader to download price data from Yahoo Finance
+import pandas_datareader as web
+# Statsmodels to run our multiple regression model
+import statsmodels.api as smf
+# To download the Fama French data from the web
+import urllib.request
+# To unzip the ZipFile
+import zipfile
 
 if __name__ == '__main__':
-    print(Hello.world())
+    print('hi')
